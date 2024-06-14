e7c463026ec7f46f14767124a55996c88ac8c106
Ciocoi Vladislav <vladislav.siloci1@gmail.com>
2024-06-14 16:43:04 +0300

    Fixed the script

--- CheckIfCUIIsRunning.sh
+++ CheckIfCUIIsRunning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the name of the tmux session
-SESSION_NAME="startcomfy_session"
+SESSION_NAME="comfy_session"
 
 # Check if any process named 'python3' is running
 if pgrep -x "python3" > /dev/null
@@ -11,11 +11,13 @@ else
     echo "python3 is not running"
 
     # Check if the tmux session already exists
-    if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+        echo "tmux session $SESSION_NAME already exists. Running startcomfy in the existing session."
+        # Run startcomfy in the existing tmux session
+        tmux send-keys -t $SESSION_NAME 'startcomfy' C-m
+    else
+        echo "tmux session $SESSION_NAME does not exist. Creating a new session and starting startcomfy."
         # Create a new tmux session and run the command
         tmux new-session -d -s $SESSION_NAME 'startcomfy'
-        echo "startcomfy is started in tmux session $SESSION_NAME"
-    else
-        echo "tmux session $SESSION_NAME already exists"
     fi
 fi
